baafd4cb87d5e758e96d962b1e507d43a6d5cff9
Gabrielvelozo <50401608+Gabrielvelozo@users.noreply.github.com>
2024-10-07 17:56:23 -0300

    Actualizar practicaParcial.py

--- Practicas 1-Parcial/practicaParcial.py
+++ Practicas 1-Parcial/practicaParcial.py
@@ -1,6 +1,7 @@
 """
 Hacer un programa que calcule la siguiente serie:
 S= 10 + 2/3 - 4/5**3 + 6/7**5 - 8/9**7 + 10/11**9 -12/13**11
+print(f"(1)**(i+1): {(-1)**(i+1)} (2*i)/ : {(2*i)} (2*i+1): {(2*i+1)} (2*i+1): {(2*i+1)} Suma= {suma}")
 """
 
 suma=10
@@ -8,7 +9,7 @@ suma=10
 for i in range (1,7):
     #1Forma Calculando el termino con el indice i.
     termino= ((-1)**(i+1))*(2*i)/(2*i+1)**(2*i+1)
-    print(f"(1)**(i+1): {(-1)**(i+1)} (2*i)/ : {(2*i)} (2*i+1): {(2*i+1)} (2*i+1): {(2*i+1)} Suma= {suma}")
+    
     suma+=termino
 print(f"suma - 1: {suma}")
 
